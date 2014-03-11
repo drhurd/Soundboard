d7b8c58df66ba4486fe3d8760124ce4923633e31
Lyndsey Scott <lyndseyscott@Lyndseys-MacBook-Air-2.local>
2014-03-11 17:19:16 -0500

    Initial Commit

---
+++ Soundboard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Soundboard
+//
+//  Created by Lyndsey Scott on 3/11/14.
+//  Copyright (c) 2014 StandableInc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SoundboardTests/SoundboardTests.m
@@ -0,0 +1,34 @@
+//
+//  SoundboardTests.m
+//  SoundboardTests
+//
+//  Created by Lyndsey Scott on 3/11/14.
+//  Copyright (c) 2014 StandableInc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SoundboardTests : XCTestCase
+
+@end
+
+@implementation SoundboardTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
